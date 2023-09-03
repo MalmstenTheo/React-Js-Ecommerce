97fdec622a3c127d910c76a6ab0dda62970c046a
MalsmtenTheo <theo.m1590@gmail.com>
2023-09-02 21:07:00 -0300

    Proyecto Final

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,17 +3,17 @@ import { NavLink, Link } from "react-router-dom"
 
 function NavBar() {
     return (
-        <section className="header">
+        <section className="d-flex align-items-center">
             <Link to="/">
-                <img src="https://img.icons8.com/?size=512&id=ISaz3cXnEkiS&format=png" alt="Logo"/>
+                <img className="h-70 w-25 justify-content-start" src="https://img.icons8.com/?size=512&id=ISaz3cXnEkiS&format=png" alt="Logo"/>
             </Link>
-            <div className="botones">
-                    <NavLink to={"/category/women's clothing"} className={({ isActive}) => isActive ? 'ActiveOption' : 'Option'}>Women's clothing</NavLink>
-                    <NavLink to={"/category/men's clothing"} className={({ isActive}) => isActive ? 'ActiveOption' : 'Option'}>Men's clothing</NavLink>
-                    <NavLink to={"/category/electronics"} className={({ isActive}) => isActive ? 'ActiveOption' : 'Option'}>Electronics</NavLink>
-                    <NavLink to={"/category/jewelery"} className={({ isActive}) => isActive ? 'ActiveOption' : 'Option'}>Jewelery</NavLink>
+            <div className="justify-content-end">
+                    <NavLink to={"/category/women's clothing"} className={({ isActive }) => (isActive ? 'btn btn-primary mx-5' : 'btn btn-outline-primary fw-bolder mx-5')}>Women's clothing</NavLink>
+                    <NavLink to={"/category/men's clothing"} className={({ isActive }) => (isActive ? 'btn btn-primary mx-5' : 'btn btn-outline-primary fw-bolder mx-5')}>Men's clothing</NavLink>
+                    <NavLink to={"/category/electronics"} className={({ isActive }) => (isActive ? 'btn btn-primary mx-5' : 'btn btn-outline-primary fw-bolder mx-5')}>Electronics</NavLink>
+                    <NavLink to={"/category/jewelery"} className={({ isActive }) => (isActive ? 'btn btn-primary mx-5' : 'btn btn-outline-primary fw-bolder mx-5')}>Jewelery</NavLink>
             </div>
-            <div className="cart">
+            <div className="">
                 <CartWidget/>
             </div>
         </section>
